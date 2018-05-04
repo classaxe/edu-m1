ed8b42e12d2432127cecada83eeb1c40ff949884
Martin Francis <martin@classaxe.com>
2018-05-04 08:15:12 -0400

    1.0.4: Changes to setup.sh to use edu-m1.classaxe.com as base address and preoprly reinstall if install is repeated.

--- setup.sh
+++ setup.sh
@@ -1,15 +1,16 @@
 #!/usr/bin/env bash
 
-echo -e "\e[32m*******************************"
+echo -e "\n\e[32m*******************************"
 echo -e "* setup.sh - setup for edu-m1 *"
-echo -e "*******************************\e[0m\n"
+echo -e "*******************************\e[0m"
 
 php_version=php5.6
+site_domain='edu-m1.classaxe.com'
 
 shopt -s expand_aliases
 source /etc/profile.d/02-php-aliases.sh
 
-echo -en "Checking php version    \e[33;1m${php_version}\e[0m                "
+echo -en "  Checking php version    \e[33;1m${php_version}\e[0m                "
 
 if $(type -t ${php_version} 2) > /dev/null; then
     echo -e "\e[32m[OK]\e[0m";
@@ -20,8 +21,8 @@ fi
 
 # Create local database schemas and users for all local sites and vhosts for them:
 sites=(
-#   'client  db_host     db_name   db_user   db_pass       php   web_host                web_aliases (space delimited)'
-    'edu-m1  localhost   edu_m1    edu_m1    Password123   5.6   edu-m1.demacmedia.com   www.edu-m1.demacmedia.com'
+#   "client  db_host     db_name   db_user   db_pass       php   web_host           web_aliases (space delimited)"
+    "edu-m1  localhost   edu_m1    edu_m1    Password123   5.6   ${site_domain}     www.${site_domain}"
 )
 
 for i in "${sites[@]}"; do
@@ -43,28 +44,30 @@ for i in "${sites[@]}"; do
         web_aliases="${web_aliases} -a ${j}"
     done;
 
-    echo -en "Creating mysql database \e[33;1m${db_name}\e[0m                "
+    echo -en "  Creating mysql database \e[33;1m${db_name}\e[0m                "
     echo "drop schema if exists ${db_name};" | MYSQL_PWD=root mysql -uroot
     echo "create schema ${db_name};" | MYSQL_PWD=root mysql -uroot
     echo -e "\e[32m[OK]\e[0m"
-    echo -en "Creating mysql user     \e[33;1m${db_user}@${db_host}\e[0m      "
+    echo -en "  Creating mysql user     \e[33;1m${db_user}@${db_host}\e[0m      "
     echo "grant all privileges on ${db_name}.* to '${db_user}'@'${db_host}' identified by '${db_pass}';" | MYSQL_PWD=root mysql -uroot
     echo -e "\e[32m[OK]\e[0m"
 
-    echo -en "Setting up apache vhost \e[33;1m${web_host}\e[0m "
+    echo -en "  Setting up apache vhost \e[33;1m${web_host}\e[0m   "
     sudo vhost add -d ${home_dir} -n ${web_host} ${web_aliases} -p ${php} -f > /dev/null 2>&1
     echo -e "\e[32m[OK]\e[0m"
-    echo -n "Restarting apache                             "
+    echo -n "  Restarting apache                             "
     sudo service apache2 restart > /dev/null 2>&1
     echo -e "\e[32m[OK]\e[0m"
 done
 
-echo -n "Installing sample data                        "
+echo -n "  Installing sample data                        "
 zcat /srv/www/edu-m1/m1.sample-data.sql.gz | MYSQL_PWD=root mysql -uroot edu_m1
 echo -e "\e[32m[OK]\e[0m"
 
-echo -n "Running Magento M1 setup                      "
+echo -n "  Running Magento M1 setup                      "
 cd "/srv/www/edu-m1/magento"
+rm -f app/etc/local.xml
+
 php5.6 -f install.php -- \
 --license_agreement_accepted "yes" \
 --locale "en_CA" \
@@ -77,10 +80,10 @@ php5.6 -f install.php -- \
 --db_prefix "" \
 --session_save "files" \
 --admin_frontname "admin" \
---url "https://edu-m1.demacmedia.com/" \
+--url "https://${site_domain}/" \
 --use_rewrites "yes" \
 --use_secure "yes" \
---secure_base_url "https://edu-m1.demacmedia.com/" \
+--secure_base_url "https://${site_domain}/" \
 --use_secure_admin "yes" \
 --admin_firstname "Admin" \
 --admin_lastname "User" \
@@ -92,8 +95,8 @@ php5.6 -f install.php -- \
 echo -e "\e[32m[OK]\e[0m\n"
 
 external_ip=$(cat /vagrant/config.yml | grep vagrant_ip | cut -d' ' -f2 | xargs)
-echo "Add the following line to your host file:"
-echo -e "\e[33;1m${external_ip}        edu-m1.demacmedia.com\e[0m\n"
-echo -e "Access the site at \e[32;1mhttps://edu-m1.demacmedia.com\e[0m\n"
-echo -e "Admin site details:\n    URL:  https://edu-m1.demacmedia.com/admin\n    User: admin\n    Pass: Password123\n"
+echo "  Add the following line to your host file:"
+echo -e "    \e[33;1m${external_ip}      ${site_domain}\e[0m\n"
+echo -e "  Access the site at:\n    \e[32;1mhttps://${site_domain}\e[0m\n"
+echo -e "  Admin site details:\n    URL:    https://${site_domain}/admin\n    User:   admin\n    Pass:   Password123\n"
 
